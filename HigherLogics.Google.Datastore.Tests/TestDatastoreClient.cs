285b62cca27c963b620372450ea6493bed064661
Michele Cantelli <emmekappa@gmail.com>
2019-08-19 14:06:23 +0200

    No transaction

--- HigherLogics.Google.Datastore.Tests/TestDatastoreClient.cs
+++ HigherLogics.Google.Datastore.Tests/TestDatastoreClient.cs
@@ -32,12 +32,14 @@ namespace HigherLogics.Google.Datastore.Tests
             if (!queryResult.Entities.Any())
                 return;
             
-            using(var transaction = db.BeginTransaction()) {
+            db.Delete(queryResult.Entities);
+            
+            /*using(var transaction = db.BeginTransaction()) {
                 
                 transaction.Delete(queryResult.Entities);
                 //transaction.Delete<T>(queryResult);
                 transaction.Commit();
-            }
+            }*/
         }
     }
 }
