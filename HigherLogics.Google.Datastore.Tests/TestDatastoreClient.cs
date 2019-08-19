88cd8c194c138faddb85ea44aea03c874b902326
Michele Cantelli <emmekappa@gmail.com>
2019-08-19 14:26:06 +0200

    Deleting by ket

--- HigherLogics.Google.Datastore.Tests/TestDatastoreClient.cs
+++ HigherLogics.Google.Datastore.Tests/TestDatastoreClient.cs
@@ -25,18 +25,19 @@ namespace HigherLogics.Google.Datastore.Tests
         public static void DeleteAllEntitiesOfKind<T>() where T : class
         {
             var db = Create();
+            
             var query = db.CreateQuery<T>();
-            var queryResult = db.RunQuery(query).Entities.ToList();
+            var queryResult = db.RunQuery(query).Entities;
             //var queryResult = db.RunQuery(db.CreateQuery<T>()).Entities<T>();
             
             if (!queryResult.Any())
                 return;
-
-            using (var transaction = db.BeginTransaction())
-            {
-                transaction.Delete(queryResult);
+            
+            using(var transaction = db.BeginTransaction()) {
+                transaction.Delete(queryResult.Select(x => x.Key));
                 //transaction.Delete<T>(queryResult);
                 transaction.Commit();
+            
             }
         }
     }
